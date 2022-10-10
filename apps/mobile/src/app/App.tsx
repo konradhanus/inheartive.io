2eacc0d52498ae33c41487b862772f5cab15a3f9
Konrad Hanus <khanus@workforcesoftware.com>
2022-10-10 15:12:51 +0200

    INHEART-114 - conflict resolved

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,17 +1,37 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
-import { Text } from 'react-native';
+import {
+  StyleSheet,
+} from 'react-native';
+import { NativeBaseProvider, Box, Button, FormControl, Stack, Input, WarningOutlineIcon } from "native-base";
 
 export const App = () => {
+
   return (
-    <>
-      <Text>Line 1</Text>
-      <Text>Line 2</Text>
-      <Text>Line 3</Text>
-      <Text>Line 4</Text>
-    </>
+    <NativeBaseProvider>
+      <Box alignItems="center">
+      <Box w="100%" maxWidth="300px">
+        <FormControl isRequired>
+          <Stack mx="4">
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" defaultValue="12345" placeholder="password" />
+            <FormControl.HelperText>
+              Must be atleast 6 characters.
+            </FormControl.HelperText>
+            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              Atleast 6 characters are required.
+            </FormControl.ErrorMessage>
+          </Stack>
+        </FormControl>
+      </Box>
+    </Box>
+      <Button>Sign in</Button>
+    </NativeBaseProvider>
   );
 };
+
+const styles = StyleSheet.create({
+
+});
 
 export default App;
 
