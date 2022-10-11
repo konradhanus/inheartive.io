b7e51d6bedcad42b40abc4bec11930d0e16f76d0
Paweł Roguski <pawel.roguski>
2022-10-11 13:06:16 +0200

    INHEART-116 - Fix button import in test

--- libs/ui/atoms/src/lib/button/Button.spec.tsx
+++ libs/ui/atoms/src/lib/button/Button.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
-import Button from './Button';
+import { Button } from './Button';
 
 describe('Button', () => {
   it('should render successfully', () => {
