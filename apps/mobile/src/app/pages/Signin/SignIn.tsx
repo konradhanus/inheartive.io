a343a854e2893c607627b2eef253aab40825da9e
Krystyna Łosieczka <44640941+krysia1@users.noreply.github.com>
2022-10-11 10:50:20 +0200

    INHEART - 114 - add routing with sample sign in and homepage pages (#4)

---
+++ apps/mobile/src/app/pages/Signin/SignIn.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, Text } from 'react-native'
+import React from 'react';
+import { Link } from 'react-router-native';
+
+
+export function SignIn() {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to SignIn!</Text>
+      <Link to="/"><Text>Back</Text></Link>
+    </SafeAreaView>
+  );
+}
+
+export default SignIn;
