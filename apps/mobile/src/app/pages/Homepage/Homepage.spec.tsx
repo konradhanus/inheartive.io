b26df02a2a39231d91fc54e7c2ea15ddd57b3cf5
Konrad Hanus <khanus@workforcesoftware.com>
2022-10-11 11:16:46 +0200

    INHEART-114 - folder name changed

--- apps/mobile/src/app/pages/Homepage/Homepage.spec.tsx
+++ apps/mobile/src/app/pages/Homepage/Homepage.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
-import Homepage from './Homepage2';
+import Homepage from './Homepage';
 
 describe('Homepage', () => {
   it('should render successfully', () => {
--- apps/mobile/src/app/pages/Homepage2/index.tsx
+++
@@ -1,3 +0,0 @@
-import Homepage from "./Homepage2";
-
-export default Homepage
