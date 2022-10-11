0510dad5232d197a30bd3bb3f746d76f4a2d1b88
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-10-11 15:18:50 +0200

    INHEART-116 - Use LoginTemplate inside App.tsx

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,42 +1,13 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import {
-  NativeBaseProvider,
-  Box,
-  FormControl,
-  Stack,
-  Input,
-  WarningOutlineIcon,
-} from 'native-base';
-import { Button } from '@inheartive/ui/atoms';
+import { NativeBaseProvider } from 'native-base';
+import { LoginTemplate } from 'libs/ui/templates/src';
 
 export const App = () => {
   return (
     <NativeBaseProvider>
-      <Box alignItems="center">
-        <Box w="100%" maxWidth="300px">
-          <FormControl isRequired>
-            <Stack mx="4">
-              <FormControl.Label>Password</FormControl.Label>
-              <Input
-                type="password"
-                defaultValue="12345"
-                placeholder="password"
-              />
-              <FormControl.HelperText>
-                Must be atleast 6 characters.
-              </FormControl.HelperText>
-              <FormControl.ErrorMessage
-                leftIcon={<WarningOutlineIcon size="xs" />}
-              >
-                Atleast 6 characters are required.
-              </FormControl.ErrorMessage>
-            </Stack>
-          </FormControl>
-        </Box>
-      </Box>
-      <Button>Sign in</Button>
+      <LoginTemplate />
     </NativeBaseProvider>
   );
 };
