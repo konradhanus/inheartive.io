d17f7d334c627ea6517cc7127506247fd2d2fb4f
Konrad Hanus <konrad.hanus@gmail.com>
2022-10-14 13:54:49 +0200

    INHEART-158: FIX storybook (#13)
    Co-authored-by: Konrad Hanus <khanus@workforcesoftware.com>

--- .storybook/story-loader.js
+++ .storybook/story-loader.js
@@ -3,9 +3,11 @@
 //
 // https://github.com/elderfo/react-native-storybook-loader.git
 
-function loadStories() {}
+function loadStories() {
+  require('../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories');
+}
 
-const stories = [];
+const stories = ['../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories'];
 
 module.exports = {
   loadStories,
