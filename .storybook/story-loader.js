09828ffc10049dff4bd27dd63f2be579e0970c24
Konrad Hanus <khanus@workforcesoftware.com>
2022-10-14 13:33:28 +0200

    INHEART-140: Add stories for Avatar

--- .storybook/story-loader.js
+++ .storybook/story-loader.js
@@ -5,9 +5,12 @@
 
 function loadStories() {
   require('../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories');
+  require('../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories');
 }
 
-const stories = ['../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories'];
+const stories = [
+  '../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories',
+  '../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories'];
 
 module.exports = {
   loadStories,
