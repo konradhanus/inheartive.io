de10f13191d2ef4ebacbff7f59e74f77881c4d79
Beata Kula <beata.kula@intive.com>
2022-10-17 16:40:43 +0200

    INHEART-144: Add theme lib, change badge stories

--- libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx
+++ libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { Badge } from '../Badge';
 import { NativeBaseProvider } from 'native-base';
+import { theme } from '@inheartive/ui/theme';
 
-storiesOf('Badge', module).add('Primary', () => (
-  <NativeBaseProvider>
-    <Badge />
-  </NativeBaseProvider>
-));
+storiesOf('Badge', module)
+  .addDecorator((story) => <NativeBaseProvider theme={theme}>{story()}</NativeBaseProvider>)
+  .add('Base', () => <Badge>NEW FEATURE</Badge>)
+  .add('colorScheme', () => (
+    <>
+      <Badge colorScheme='success'>NEW FEATURE - SUCCESS</Badge>
+      <Badge colorScheme='danger'>NEW FEATURE - DANGER</Badge>
+      <Badge colorScheme='info'>INFO</Badge>
+      <Badge colorScheme='warning'>WARNING</Badge>
+      <Badge colorScheme='coolGray'>DARK</Badge>
+      <Badge colorScheme='fuchsia'>FUCHSIA</Badge>
+      <Badge colorScheme='primary'>PRIMARY</Badge>
+      <Badge colorScheme='secondary'>SECONDARY</Badge>
+    </>
+  ));
