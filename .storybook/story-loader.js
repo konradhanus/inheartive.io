cd1870a856650381ad3a7f74326b200cb800cc43
Beata Kula <beata.kula@intive.com>
2022-10-17 10:55:54 +0200

    INHEART-144: Add loadStories

--- .storybook/story-loader.js
+++ .storybook/story-loader.js
@@ -5,9 +5,13 @@
 
 function loadStories() {
   require('../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories');
+  require('../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx');
 }
 
-const stories = ['../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories'];
+const stories = [
+  '../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories',
+  '../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx',
+];
 
 module.exports = {
   loadStories,
