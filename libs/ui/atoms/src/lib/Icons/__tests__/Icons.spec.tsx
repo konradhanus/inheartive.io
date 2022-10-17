4012d7e9483dface5594df01a9eb608000ed9838
KonradKarlinski <konrad.karlinski@intive.com>
2022-10-17 17:14:36 +0200

    INHEART-142: Added jest-each dependency and modify test for icons

--- libs/ui/atoms/src/lib/Icons/__tests__/Icons.spec.tsx
+++ libs/ui/atoms/src/lib/Icons/__tests__/Icons.spec.tsx
@@ -3,12 +3,45 @@ import { render, screen, getQueriesForElement } from '@testing-library/react-nat
 
 import { Icons } from '../Icons';
 import { NativeBaseProvider } from 'native-base';
+import each from 'jest-each';
 
 describe('Icons', () => {
-  it('should render successfully menu icon', () => {
+  each([
+    'add',
+    'arrow-back',
+    'arrow-forward',
+    'arrow-up',
+    'arrow-down',
+    'check',
+    'check-circle',
+    'chevron-down',
+    'chevron-left',
+    'chevron-right',
+    'chevron-up',
+    'circle',
+    'close',
+    'small-close',
+    'menu',
+    'info',
+    'info-outline',
+    'minus',
+    'moon',
+    'question',
+    'question-outline',
+    'search',
+    'sun',
+    'warning-1',
+    'warning-2',
+    'warning-outline',
+    'three-dots',
+    'share',
+    'play',
+    'favourite',
+    'delete',
+  ]).test('should render successfully menu icons', (iconName) => {
     const { container } = render(
       <NativeBaseProvider>
-        <Icons iconName='menu' />
+        <Icons iconName={iconName} />
       </NativeBaseProvider>
     );
     expect(container).toBeTruthy();
