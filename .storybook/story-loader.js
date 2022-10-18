934427de4b98dc9a223456398d2a42cfcb3929d3
Beata Kula <beata.kula@intive.com>
2022-10-18 15:04:06 +0200

    INHEART-139: Add TextLogo, add assets library

--- .storybook/story-loader.js
+++ .storybook/story-loader.js
@@ -8,6 +8,7 @@ function loadStories() {
   require('../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx');
   require('../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories');
   require('../libs/ui/atoms/src/lib/Text/__stories__/Text.stories');
+  require('../libs/ui/atoms/src/lib/TextLogo/__stories__/TextLogo.stories');
 }
 
 const stories = [
@@ -15,6 +16,7 @@ const stories = [
   '../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx',
   '../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories',
   '../libs/ui/atoms/src/lib/Text/__stories__/Text.stories',
+  '../libs/ui/atoms/src/lib/TextLogo/__stories__/TextLogo.stories',
 ];
 
 module.exports = {
---
+++ libs/ui/atoms/src/lib/TextLogo/TextLogo.tsx
@@ -0,0 +1,8 @@
+import { Image } from '@inheartive/ui/atoms';
+import { textLogo } from '@inheartive/ui/assets';
+
+export const TextLogo = () => {
+  return <Image source={textLogo} alt='Text logo' />;
+};
+
+export default TextLogo;
