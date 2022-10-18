9fa1cf9f9dd4afb27313807b3e720dcdf947ba81
BeataKula <101329582+BeataKula@users.noreply.github.com>
2022-10-18 15:43:27 +0200

    Inheart 144 : Atom - Badge (#20)
    * INHEART-144: Add Badge atom
    
    * INHEART-158: FIX storybook (#13)
    
    Co-authored-by: Konrad Hanus <khanus@workforcesoftware.com>
    
    * INHEART-144:  Fix Badge atom tes import
    
    * INHEART-144: Add Badge stories
    
    * INHEART-144: Add loadStories
    
    * INHEART-144: Add theme lib, change badge stories
    
    * INHEART-144: Add Variants to Badge stories
    
    * INHEART-144: Add Rounded, Background and Example
    
    Co-authored-by: Beata Kula <beata.kula@intive.com>
    Co-authored-by: Konrad Hanus <konrad.hanus@gmail.com>
    Co-authored-by: Konrad Hanus <khanus@workforcesoftware.com>

--- .storybook/story-loader.js
+++ .storybook/story-loader.js
@@ -5,14 +5,17 @@
 
 function loadStories() {
   require('../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories');
+  require('../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx');
   require('../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories');
   require('../libs/ui/atoms/src/lib/Text/__stories__/Text.stories');
 }
 
 const stories = [
   '../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories',
+  '../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories.tsx',
   '../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories',
-  '../libs/ui/atoms/src/lib/Text/__stories__/Text.stories'];
+  '../libs/ui/atoms/src/lib/Text/__stories__/Text.stories',
+];
 
 module.exports = {
   loadStories,
