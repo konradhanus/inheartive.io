a1e28c40395645ce8f365fe2482e8be422e987ff
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-10-21 13:56:38 +0200

    INHEART-152: Add data lib with categories

---
+++ libs/data/src/categories/categories.mock.ts
@@ -0,0 +1,16 @@
+import { ICategory } from './category';
+
+export const categoriesMock: ICategory[] = [
+  {
+    id: 'food',
+    name: 'Food',
+  },
+  {
+    id: 'homemade',
+    name: 'Homemade',
+  },
+  {
+    id: 'learning',
+    name: 'Learning',
+  },
+];
