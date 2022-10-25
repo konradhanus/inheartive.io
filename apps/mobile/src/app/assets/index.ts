1073287867b2e92d6a5472f733309d4376c7ff2d
Beata Kula <beata.kula@intive.com>
2022-10-25 14:27:51 +0200

    Revert "INHEART-151: Add AuctionHearts molecule"
    This reverts commit 157242428d8cacba8799ea9d68724d59ff58a1cb.

--- apps/mobile/src/app/assets/index.ts
+++ apps/mobile/src/app/assets/index.ts
@@ -1,5 +1,4 @@
 import logo from './images/logo.png';
 import textLogo from './images/text-logo.png';
-import vector from './images/vector.png';
 
-export { logo, textLogo, vector };
+export { logo, textLogo };
