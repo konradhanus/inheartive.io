5b624330c8fe1578979fef70ea20d00cce362376
Paweł Roguski <kontakt@pawelroguski.pl>
2022-10-27 12:55:28 +0200

    INHEART-215: Add additional props to Icon atom
    Additional props:
    - color
    - backgroundColor
    - borderRadius
    - onPress
    - iconStyle

--- libs/ui/atoms/src/lib/Icon/__stories__/Icon.stories.tsx
+++ libs/ui/atoms/src/lib/Icon/__stories__/Icon.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import { IconType, Icon } from '../Icon';
@@ -10,8 +11,13 @@ storiesOf('Icon', module)
     <Row px={8} space={4} mt={4} flexWrap='wrap'>
       {Object.values(IconType).map((name, index) => (
         <View mt={4} key={index}>
-          <Icon name={name} />
+          <Icon size={17} name={name} />
         </View>
       ))}
     </Row>
+  ))
+  .add('Custom properties', () => (
+    <View>
+      <Icon name={IconType.favorite} size={200} color='#960e8a' />
+    </View>
   ));
