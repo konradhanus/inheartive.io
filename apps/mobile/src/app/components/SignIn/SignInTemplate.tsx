daf8ac2dced4264a989f3775841c05b36796eedc
Paweł Roguski <kontakt@pawelroguski.pl>
2022-10-28 13:24:27 +0200

    INHEART-157: Fix scrolling screen
    Fix problem with scrolling screen when keyboard appear

--- apps/mobile/src/app/components/SignIn/SignInTemplate.tsx
+++ apps/mobile/src/app/components/SignIn/SignInTemplate.tsx
@@ -2,29 +2,34 @@
 import React from 'react';
 
 import { PixelRatio, StyleSheet } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import { Box, Image } from '@inheartive/ui/atoms';
 import { LoginFormControl } from '@inheartive/ui/molecules';
-import { View } from 'native-base';
-import { logo } from './../../assets/index';
+import { logo } from '../../assets/index';
 
 export function SignInTemplate() {
   return (
-    <View style={styles.container}>
+    <KeyboardAwareScrollView
+      resetScrollToCoords={{ x: 0, y: 0 }}
+      contentContainerStyle={styles.container}
+      scrollEnabled={false}
+    >
       <Image source={logo} alt='Logo' />
 
-      <Box w='100%' mt='50'>
+      <Box w='100%' mt='10'>
         <LoginFormControl />
       </Box>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: PixelRatio.getPixelSizeForLayoutSize(16),
-    paddingTop: PixelRatio.getPixelSizeForLayoutSize(50),
-    height: '100%',
+    paddingTop: PixelRatio.getPixelSizeForLayoutSize(10),
     alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
   },
 });
 
