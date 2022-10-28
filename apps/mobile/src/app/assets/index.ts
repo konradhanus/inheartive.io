db9b39811400b5c88685d6df39611ef5702689c3
Beata Kula <beata.kula@intive.com>
2022-10-28 13:33:10 +0200

    INHEART-154: Resolve notes fix

--- apps/mobile/src/app/assets/index.ts
+++ apps/mobile/src/app/assets/index.ts
@@ -1,7 +1,6 @@
 import logo from './images/logo.png';
 import textLogo from './images/text-logo.png';
 import auctionHeart from './images/auction-heart.png';
-import heartCredit from './images/heart-credit.png';
 import placeholder from './images/placeholder.png';
 
-export { logo, textLogo, auctionHeart, heartCredit, placeholder };
+export { logo, textLogo, auctionHeart, placeholder };
