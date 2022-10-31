d499cc68f299006957cd2c56648223580d96d747
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-10-31 11:57:54 +0100

    INHEART-243: Add Sort story

--- .storybook/story-loader.js
+++ .storybook/story-loader.js
@@ -7,6 +7,7 @@ function loadStories() {
   require('../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories');
   require('../libs/ui/molecules/src/lib/AuctionAuthor/__stories__/AuctionAuthor.stories');
   require('../libs/ui/molecules/src/lib/CategoryFilter/__stories__/CategoryFilter.stories');
+  require('../libs/ui/molecules/src/lib/Sort/__stories__/Sort.stories');
   require('../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories');
   require('../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories');
   require('../libs/ui/atoms/src/lib/Text/__stories__/Text.stories');
@@ -21,6 +22,7 @@ const stories = [
   '../libs/ui/molecules/src/lib/LoginFormControl/__stories__/LoginFormControl.stories',
   '../libs/ui/molecules/src/lib/AuctionAuthor/__stories__/AuctionAuthor.stories',
   '../libs/ui/molecules/src/lib/CategoryFilter/__stories__/CategoryFilter.stories',
+  '../libs/ui/molecules/src/lib/Sort/__stories__/Sort.stories',
   '../libs/ui/atoms/src/lib/Badge/__stories__/Badge.stories',
   '../libs/ui/atoms/src/lib/Avatar/__stories__/Avatar.stories',
   '../libs/ui/atoms/src/lib/Text/__stories__/Text.stories',
