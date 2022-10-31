00b0d33f7dedbc0bf889de494065e85544ac380a
pawelroguski <97616451+pawelroguski@users.noreply.github.com>
2022-10-31 11:41:55 +0100

    Inheart 157 - Fix scrolling screen when keyboard appear (#41)
    * INHEART-157: Add solution for ios not showing keyboard
    
    * INHEART-157: Add solution for ios not showing keyboard
    
    * INHEART-157: Fix scrolling screen
    
    Fix problem with scrolling screen when keyboard appear
    
    Co-authored-by: KonradKarlinski <konrad.karlinski@intive.com>

--- apps/mobile/src/app/components/SignIn/SignInPage.tsx
+++ apps/mobile/src/app/components/SignIn/SignInPage.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { View, ScrollView, SafeAreaView } from 'react-native';
+
 import SignInTemplate from './SignInTemplate';
 
 export function SignInPage() {
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <SignInTemplate />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <SignInTemplate />;
 }
 
 export default SignInPage;
