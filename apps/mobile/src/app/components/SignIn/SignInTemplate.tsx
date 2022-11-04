444c7689c5edfbc0038ef3e719f441e9e28f4c98
Paweł Roguski <kontakt@pawelroguski.pl>
2022-11-04 13:00:17 +0100

    INHEART-145: Deletes some garbage from SignInTemplate

--- apps/mobile/src/app/components/SignIn/SignInTemplate.tsx
+++ apps/mobile/src/app/components/SignIn/SignInTemplate.tsx
@@ -1,30 +1,12 @@
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import React from 'react';
-
 import { PixelRatio, StyleSheet } from 'react-native';
-
 import { Box, Image } from '@inheartive/ui/atoms';
 import { LoginFormControl } from '@inheartive/ui/molecules';
 import { View } from 'native-base';
 import { logo } from './../../assets/index';
-import { AvatarDropdown } from '@inheartive/ui/molecules';
 
 export function SignInTemplate() {
-  const data = [
-    {
-      name: 'Link 1',
-      route: '/',
-    },
-    {
-      name: 'Link 2',
-      route: '/auctions',
-    },
-    {
-      name: 'Link 3',
-      route: '/signin',
-    },
-  ];
-
   return (
     <View style={styles.container}>
       <Image source={logo} alt='Logo' />
@@ -32,7 +14,6 @@ export function SignInTemplate() {
       <Box w='100%' mt='50'>
         <LoginFormControl />
       </Box>
-      <AvatarDropdown dropdownList={data} />
     </View>
   );
 }
