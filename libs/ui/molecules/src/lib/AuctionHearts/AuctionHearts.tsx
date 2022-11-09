72c9a82e2950986ff9370770488fb18811056a70
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-11-09 13:52:52 +0100

    INHEART-148: Add index files, refactor

--- libs/ui/molecules/src/lib/AuctionHearts/AuctionHearts.tsx
+++ libs/ui/molecules/src/lib/AuctionHearts/AuctionHearts.tsx
@@ -3,25 +3,17 @@ import React from 'react';
 import { Image } from '@inheartive/ui/atoms';
 import { auctionHeart } from '@inheartive/assets';
 import { Text, Row } from '@inheartive/ui/atoms';
-import styled from 'styled-components/native';
-
-const TextStyled = styled(Text)`
-  top: -2;
-`;
 interface IAuctionHeartsProps {
   quantity: number;
-  testID?: string | undefined;
 }
 
 function AuctionHearts(props: IAuctionHeartsProps) {
-  const { quantity, testID } = props;
+  const { quantity } = props;
 
   return (
     <Row space={2} alignItems='center'>
-      <Image source={auctionHeart} alt='auction hearts' />
-      <TextStyled fontSize='xl' testID={testID}>
-        {quantity}
-      </TextStyled>
+      <Text>{quantity}</Text>
+      <Image maxHeight={'100%'} source={auctionHeart} alt='Heartcoins' />
     </Row>
   );
 }
