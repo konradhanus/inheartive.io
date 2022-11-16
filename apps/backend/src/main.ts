77f4e532a4c962a1f4083fd2ab097c6595fede7e
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-11-16 19:33:43 +0100

    INHEART-282: Restore transform property in ValidationPipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
       transformOptions: {
         enableImplicitConversion: true,
       },
