404640de59e9247c4d5f7ff5594b200b7679847a
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-11-16 19:07:29 +0100

    INHEART-282: Remove transform propery from ValidationPipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -16,7 +16,6 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true,
       transformOptions: {
         enableImplicitConversion: true,
       },
