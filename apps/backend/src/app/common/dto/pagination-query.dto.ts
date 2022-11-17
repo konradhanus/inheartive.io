250b573c3b6057003af3a1b46aa55972ac6cd6dd
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-11-17 14:35:08 +0100

    INHEART-282: Add transforming pagination query, add validation rules

--- apps/backend/src/app/common/dto/pagination-query.dto.ts
+++ apps/backend/src/app/common/dto/pagination-query.dto.ts
@@ -1,12 +1,17 @@
-import { IsOptional, IsPositive, Max } from 'class-validator';
+import { IsNumber, IsOptional, IsPositive, Max, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class PaginationQueryDto {
+  @Transform(({ value }) => +value)
   @IsOptional()
+  @IsNumber()
   @IsPositive()
   @Max(50)
   limit: number;
 
+  @Transform(({ value }) => +value)
   @IsOptional()
-  @IsPositive()
+  @IsNumber()
+  @Min(0)
   offset: number;
 }
