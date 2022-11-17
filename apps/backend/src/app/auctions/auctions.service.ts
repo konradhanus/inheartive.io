93372757883b92b98e28ae0b13adc91f75e0a098
pmusialik-intive <115072317+pmusialik-intive@users.noreply.github.com>
2022-11-17 23:53:53 +0100

    Inheart 282 - Add categories, update auctions (#52) (#54)
    * INHEART-282: Add validation packages
    
    * INHEART-282: Add validation pipe
    
    * INHEART-282: Add validation rules to auction
    
    * INHEART-282: Add categories
    
    * INHEART-282: Add pagination dto
    
    * INHEART-282: Add categories to auctions, add pagination
    
    * INHEART-282: Add not found err interceptor
    
    * INHEART-282: Add ordering by ID
    
    * INHEART-282: Add throwing validation http exceptions
    
    * INHEART-282: Add date fields to entities
    
    * INHEART-282: Remove manual dealing with errors
    
    * INHEART-282: Add get auctions limit if not set
    
    * INHEART-282: Add max limit to pagination query
    
    * INHEART-282: Remove transform propery from ValidationPipe
    
    * INHEART-282: Restore transform property in ValidationPipe
    
    * INHEART-282: Add maximum lengths and description field
    
    * INHEART-282: Add minimum lengths
    
    * INHEART-282: Clean up
    
    * INHEART-282: Change findAll order
    
    * INHEART-282: Refactor
    
    * INHEART-282: Use Nest logger instead of console
    
    * INHEART-282: Remove transform, add useContainer
    
    * INHEART-282: Add CategoryExist validation rule
    
    * INHEART-282: Use UUID, add minPrice
    
    * INHEART-282: Add transforming pagination query, add validation rules
    
    * INHEART-282: Set enableImplicitConversion to false

--- apps/backend/src/app/auctions/auctions.service.ts
+++ apps/backend/src/app/auctions/auctions.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { EntityNotFoundError, Repository } from 'typeorm';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { CreateAuctionDto } from './dto/create-auction.dto';
 import { UpdateAuctionDto } from './dto/update-auction.dto';
 import { Auction } from './entities/auction.entity';
+
+const MAX_LIMIT = 50;
 
 @Injectable()
 export class AuctionsService {
@@ -18,39 +21,38 @@ export class AuctionsService {
     return this.auctionsRepository.save(auction);
   }
 
-  findAll() {
-    return this.auctionsRepository.find();
+  findAll(paginationQuery: PaginationQueryDto) {
+    const { limit, offset } = paginationQuery;
+
+    return this.auctionsRepository.find({
+      relations: ['category'],
+      skip: offset,
+      take: limit || MAX_LIMIT,
+      order: {
+        createdAt: 'DESC',
+      },
+    });
   }
 
-  async findOne(id: number) {
-    const auction = await this.auctionsRepository.findOneBy({ id });
-
-    if (!auction) {
-      throw new NotFoundException(`Auction #${id} not found`);
-    }
-
-    return auction;
+  findOne(id: string) {
+    return this.auctionsRepository.findOneOrFail({ where: { id }, relations: ['category'] });
   }
 
-  async update(id: number, updateAuctionDto: UpdateAuctionDto) {
+  async update(id: string, updateAuctionDto: UpdateAuctionDto) {
     const auction = await this.auctionsRepository.preload({
       id,
       ...updateAuctionDto,
     });
 
     if (!auction) {
-      throw new NotFoundException(`Auction #${id} not found`);
+      throw new EntityNotFoundError(Auction, { id });
     }
 
     return this.auctionsRepository.save(auction);
   }
 
-  async remove(id: number) {
-    const auction = await this.auctionsRepository.findOneBy({ id });
-
-    if (!auction) {
-      throw new NotFoundException(`Auction #${id} not found`);
-    }
+  async remove(id: string) {
+    const auction = await this.auctionsRepository.findOneByOrFail({ id });
 
     return this.auctionsRepository.remove(auction);
   }
