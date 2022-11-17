8b7138a0351f3209e57e8c4879360e86984bccc2
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-11-17 01:04:13 +0100

    INHEART-282: Add CategoryExist validation rule

--- apps/backend/src/app/categories/categories.module.ts
+++ apps/backend/src/app/categories/categories.module.ts
@@ -4,10 +4,11 @@ import { CategoriesController } from './categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Auction } from '../auctions/entities/auction.entity';
+import { CategoryExistsRule } from './validators/category-exists';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category, Auction])],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, CategoryExistsRule],
 })
 export class CategoriesModule {}
