06a2f6593844e8106c10bceca0f9681da3e79578
Konrad Hanus <konrad.hanus@intive.com>
2022-11-17 07:56:01 +0100

    INHEART-93: Change project's package name

--- apps/mobile/ios/inHEARTiveTests/inHEARTiveTests.m
+++ apps/mobile/ios/inHEARTiveTests/inHEARTiveTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MobileTests : XCTestCase
+@interface inHEARTiveTests : XCTestCase
 
 @end
 
-@implementation MobileTests
+@implementation inHEARTiveTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
