3fa941292c4e41137593440e6bed740f44739cb4
Misza Pawlik <ppawlik@workforcesoftware.com>
2022-12-14 10:58:30 +0100

    INHEART-146: Adjust theme color in AppHeader

--- libs/ui/organisms/src/lib/AppHeader/AppHeader.tsx
+++ libs/ui/organisms/src/lib/AppHeader/AppHeader.tsx
@@ -3,7 +3,7 @@ import { Avatar, Row, TextLogo, textLogoColor, View } from '@inheartive/ui/atoms
 
 function AppHeader() {
   return (
-    <Row px={8} py={2} justifyContent={'space-between'} bg='secondary.600' alignItems='center'>
+    <Row px={8} py={2} justifyContent={'space-between'} bg='primary.500' alignItems='center'>
       <TextLogo width={150} color={textLogoColor.inverted} />
       <Avatar size={'lg'} />
     </Row>
