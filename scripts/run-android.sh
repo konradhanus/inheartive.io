f1dc078b9536f46c74b972d5b46538afb97bd6d5
KonradKarlinski <konrad.karlinski@intive.com>
2022-12-21 23:00:28 +0100

    INHEART-320: Added script to run ios and android and updated README.md

--- scripts/run-android.sh
+++ scripts/run-android.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# This script clear cache as first step, next run docker with backend enviroment and at the end run android
 nx clear-cache & docker-compose up -d & yarn start:android
--- scripts/run-ios.sh
+++ scripts/run-ios.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# This script clear cache as first step, next run docker with backend enviroment and at the end run ios
 nx clear-cache & docker-compose up -d & yarn start:ios
