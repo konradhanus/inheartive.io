448e7d7170f345a2ffcee7b296a5a5b27ce054b2
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-12-22 11:23:20 +0100

    INHEART-148: Revert registerComponent to Mobile

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import AppRoot from './app/App';
 
-AppRegistry.registerComponent('inHEARTive', () => AppRoot);
+AppRegistry.registerComponent('Mobile', () => AppRoot);
