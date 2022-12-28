769f119993efd8a15ec45d79abc4fb2e2738c9cc
Przemek Musialik <przemyslaw.musialik@intive.com>
2022-12-28 13:58:49 +0100

    INHEART-322: Set testID for footer, use some links

--- libs/ui/organisms/src/lib/FooterMenu/FooterMenu.tsx
+++ libs/ui/organisms/src/lib/FooterMenu/FooterMenu.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
 
 import React, { useState } from 'react';
-import HeartsCredits from '../../../../molecules/src/lib/HeartsCredits';
 import { Icon, IconType, Pressable } from '@inheartive/ui/atoms';
+import { HeartsCredits } from '@inheartive/ui/molecules';
 import { FooterMenuProps, IconNameType } from './FooterMenuTypes';
 import { HStack } from 'native-base';
+import { Link } from 'react-router-native';
 
-const FooterMenu: React.ComponentType<FooterMenuProps> = ({ testID, activeIcon }) => {
+const FooterMenu: React.ComponentType<FooterMenuProps> = ({ activeIcon }) => {
   const activeColor = 'black';
   const inActiveColor = 'gray.300';
   const [currentActiveIcon, setCurrentActiveIcon] = useState(activeIcon);
@@ -17,19 +18,19 @@ const FooterMenu: React.ComponentType<FooterMenuProps> = ({ testID, activeIcon }
   };
 
   return (
-    <HStack bg='white' safeAreaBottom justifyContent='space-between' px='4' alignItems='center' testID={testID}>
-      <Pressable p={1} onPress={() => onFooterMenuChange(IconNameType.homepage)}>
+    <HStack bg='white' justifyContent='space-between' px='4' alignItems='center' testID='footer-menu'>
+      <Link to='/'>
         <Icon name={IconType.homeOutline} size={50} color={getIconColor(IconNameType.homepage)} />
-      </Pressable>
+      </Link>
       <Pressable p={1} onPress={() => onFooterMenuChange(IconNameType.search)}>
         <Icon name={IconType.search} size={50} color={getIconColor(IconNameType.search)} />
       </Pressable>
       <Pressable p={1} onPress={() => onFooterMenuChange(IconNameType.heart)}>
         <HeartsCredits credit={99} size={60} />
       </Pressable>
-      <Pressable p={1} onPress={() => onFooterMenuChange(IconNameType.plus)}>
+      <Link to='/auctions/create'>
         <Icon name={IconType.plusCircle} size={50} color={getIconColor(IconNameType.plus)} />
-      </Pressable>
+      </Link>
       <Pressable p={1} onPress={() => onFooterMenuChange(IconNameType.star)}>
         <Icon name={IconType.starOutline} size={50} color={getIconColor(IconNameType.star)} />
       </Pressable>
--- libs/ui/organisms/src/lib/FooterMenu/__tests__/FooterMenu.spec.tsx
+++ libs/ui/organisms/src/lib/FooterMenu/__tests__/FooterMenu.spec.tsx
@@ -3,12 +3,10 @@ import { FooterMenu } from '../FooterMenu';
 import { IconNameType } from '../FooterMenuTypes';
 
 describe('FooterMenu', () => {
-  const testID = 'FooterMenuId';
-
   it('should render successfully footer menu', () => {
     const activeIcon = IconNameType.star;
-    const { getByTestId } = render(<FooterMenu testID={testID} activeIcon={activeIcon} />);
+    const { getByTestId } = render(<FooterMenu activeIcon={activeIcon} />);
 
-    expect(getByTestId(testID)).toBeTruthy();
+    expect(getByTestId('footer-menu')).toBeTruthy();
   });
 });
