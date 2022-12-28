9f6073a0167c94758c4b7acd6fa83510cd8a8ee0
BeataKula <101329582+BeataKula@users.noreply.github.com>
2022-12-28 10:04:30 +0100

    INHEART-155: Template - AuctionList  (#62)
    * INHEART-155: Template - AuctionList - part 1
    
    * INHEART-155: Template - AuctionList - part 1 v2
    
    * INHEART-155: Template - AuctionList - part 2
    
    * INHEART-155: Template - AuctionList - change filter name
    
    * INHEART-155: Template - AuctionList - part 4 - some cosmetics changes
    
    * INHEART-155: Template - AuctionList - part 5 - code review coments
    
    * INHEART-155: Template - AuctionList - part 6 - Actionsheet.Item styles
    
    * INHEART-155: Template - AuctionList - part 7 - Actionsheet.Item styles
    
    Co-authored-by: Beata Kula <beata.kula@intive.com>

--- libs/ui/molecules/src/lib/CategoryFilter/CategoryFilter.tsx
+++ libs/ui/molecules/src/lib/CategoryFilter/CategoryFilter.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Row, Select, ISelectItemProps, Text } from '@inheartive/ui/atoms';
+import { Row, ISelectItemProps, Text, Pressable, Icon, IconType } from '@inheartive/ui/atoms';
+import { theme } from '@inheartive/ui/theme';
+import { Actionsheet, useDisclose } from 'native-base';
 
 interface ICategoryFilterProps {
   items: ISelectItemProps[];
@@ -9,25 +11,51 @@ interface ICategoryFilterProps {
 
 function CategoryFilter(props: ICategoryFilterProps) {
   const { onChange, selectedValue, items } = props;
+  const { isOpen, onOpen, onClose } = useDisclose();
+
+  const CategoryFilterButton = (props: { onOpen: () => void }) => {
+    const onOpen: () => void = props.onOpen;
+
+    const ButtonText = () => {
+      return (
+        <Text fontSize='lg' color={theme.colors.trueGray['600']}>
+          Filter
+        </Text>
+      );
+    };
+
+    return (
+      <Pressable onPress={onOpen}>
+        <Row space={3} alignItems='center'>
+          <ButtonText />
+          <Icon name={IconType.chevronDown} size={25} color={theme.colors.trueGray['500']} />
+        </Row>
+      </Pressable>
+    );
+  };
 
   return (
-    <Row display={'flex'} alignItems={'center'}>
-      <Text width={70}>Category:</Text>
-      <Select
-        borderColor={'transparent'}
-        selectedValue={selectedValue}
-        width={170}
-        accessibilityLabel='Category'
-        color='black'
-        size={16}
-        display='flex'
-        flexGrow={1}
-        onValueChange={(itemValue) => onChange && onChange(itemValue)}
-      >
-        {items.map((category) => (
-          <Select.Item key={category.value} label={category.label} value={category.value} />
-        ))}
-      </Select>
+    <Row display={'flex'} alignItems={'center'} ml={3} mt={2} mb={2}>
+      <CategoryFilterButton onOpen={onOpen} />
+      <Actionsheet isOpen={isOpen} onClose={onClose}>
+        <Actionsheet.Content>
+          <Actionsheet.Item isDisabled>Show</Actionsheet.Item>
+          {items.map((category) => (
+            <Actionsheet.Item
+              key={category.value}
+              onPressIn={() => {
+                onChange && onChange(category.value);
+              }}
+            >
+              {category.value === selectedValue ? (
+                <Text underline> {category.label}</Text>
+              ) : (
+                <Text>{category.label}</Text>
+              )}
+            </Actionsheet.Item>
+          ))}
+        </Actionsheet.Content>
+      </Actionsheet>
     </Row>
   );
 }
