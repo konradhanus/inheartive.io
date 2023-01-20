6bab44112946ac9b2ffa2cee00172c00cc2f04cf
Serhii Bilyk <serhii.bilyk@intive.com>
2023-01-20 09:54:14 +0100

    INHEART-336: Validate intive email and update TS version

--- apps/mobile/src/app/components/Register/RegisterTemplate.tsx
+++ apps/mobile/src/app/components/Register/RegisterTemplate.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import { FormControl, Input, Button, ScrollView, Column } from '@inheartive/ui/atoms';
-import { UseFormRegister, UseFormHandleSubmit, FieldErrors, Controller, Control } from 'react-hook-form';
+import {
+  UseFormRegister,
+  UseFormHandleSubmit,
+  FieldErrors,
+  Controller,
+  Control,
+  FieldValues,
+  UseControllerProps,
+} from 'react-hook-form';
 import { RegisterFormValues } from './register-form-values';
 import { RoutingPath } from '../../routing';
 import { useNavigate } from 'react-router-native';
 
-interface Props {
-  control: Control<RegisterFormValues>;
-  register: UseFormRegister<RegisterFormValues>;
-  handleSubmit: UseFormHandleSubmit<RegisterFormValues>;
-  errors: FieldErrors<RegisterFormValues>;
-  onSubmit: (data) => void;
+interface Props<T extends FieldValues> {
+  control: Control<T>;
+  register: UseFormRegister<T>;
+  handleSubmit: UseFormHandleSubmit<T>;
+  errors: FieldErrors<T>;
+  onSubmit: (data: T) => void;
 }
+type FormRulesStrategy = {
+  [Prop in keyof RegisterFormValues]: UseControllerProps['rules'];
+};
 
-export function RegisterTemplate(props: Props) {
+const FORM_RULES_STRATEGY = {
+  email: {
+    required: 'Email name is required',
+    minLength: 2,
+    pattern: {
+      value: /.*@intive.com$/,
+      message: 'Only intive email is allowed',
+    },
+  },
+  firstName: { required: 'First name is required', minLength: 2 },
+  lastName: { required: 'Last name is required', minLength: 2 },
+  password: { required: 'Password is required', minLength: { value: 8, message: 'Min 8!' } },
+} as const satisfies FormRulesStrategy;
+
+export function RegisterTemplate(props: Props<RegisterFormValues>) {
   const { control, handleSubmit, errors, onSubmit } = props;
 
   const navigate = useNavigate();
@@ -34,7 +59,7 @@ export function RegisterTemplate(props: Props) {
               />
             )}
             name='email'
-            rules={{ required: 'Email name is required', minLength: 2 }}
+            rules={FORM_RULES_STRATEGY.email}
             defaultValue=''
           />
           <FormControl.ErrorMessage>{errors.email?.message}</FormControl.ErrorMessage>
@@ -48,7 +73,7 @@ export function RegisterTemplate(props: Props) {
               <Input onBlur={onBlur} placeholder='John' onChangeText={(val) => onChange(val)} value={value} />
             )}
             name='firstName'
-            rules={{ required: 'First name is required', minLength: 2 }}
+            rules={FORM_RULES_STRATEGY.firstName}
             defaultValue=''
           />
           <FormControl.ErrorMessage>{errors.firstName?.message}</FormControl.ErrorMessage>
@@ -62,7 +87,7 @@ export function RegisterTemplate(props: Props) {
               <Input onBlur={onBlur} placeholder='Doe' onChangeText={(val) => onChange(val)} value={value} />
             )}
             name='lastName'
-            rules={{ required: 'Last name is required', minLength: 2 }}
+            rules={FORM_RULES_STRATEGY.lastName}
             defaultValue=''
           />
           <FormControl.ErrorMessage>{errors.lastName?.message}</FormControl.ErrorMessage>
@@ -76,7 +101,7 @@ export function RegisterTemplate(props: Props) {
               <Input type='password' onBlur={onBlur} onChangeText={(val) => onChange(val)} value={value} />
             )}
             name='password'
-            rules={{ required: 'Password is required', minLength: { value: 8, message: 'Min 8!' } }}
+            rules={FORM_RULES_STRATEGY.password}
           />
           <FormControl.ErrorMessage>{errors.password?.message}</FormControl.ErrorMessage>
         </FormControl>
