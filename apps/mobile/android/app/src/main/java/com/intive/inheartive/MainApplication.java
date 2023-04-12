ba9e63483ae671fab37d9c416ab0a1f2eca5be9d
kwarantyna <45069068+kwarantyna@users.noreply.github.com>
2023-04-12 19:22:57 +0200

    INHEART-446: Added force override for only lightmode (#161)
    * INHEART-446: Added force override for only lightmode
    
    * INHEART-446: Forced lightmode on Android
    
    ---------
    
    Co-authored-by: Afrodyta Różycka <afrodyta.rozycka@intive.com>

--- apps/mobile/android/app/src/main/java/com/intive/inheartive/MainApplication.java
+++ apps/mobile/android/app/src/main/java/com/intive/inheartive/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.soloader.SoLoader;
 import com.intive.inheartive.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -57,6 +58,7 @@ public class MainApplication extends Application implements ReactApplication {
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
   }
 
   /**
