ee9a466e7afce3a250f8291785804732b899dea8
pawelroguski <97616451+pawelroguski@users.noreply.github.com>
2023-04-14 19:14:44 +0200

    INHEART-447: Update auction price during bidding (#163)
    * INHEART-447: Update auction price during bidding
    
    INHEART-447: Update auction price during bidding
    
    Add transaction to bidding action for security reason. This feature provide additional update for auction table apart of bids table update. Transaction was implemented mainly  to avoid update only one of them.
    
    * INHEART-447: Improve error supporting
    
    * INHEART-447: Improve error supporting
    
    * INHEART-447: Add validator to create auction DTO
    
    ---------
    
    Co-authored-by: Paweł Roguski <pawel.roguski@intive.com>

--- apps/backend/src/app/auctions/auctions.module.ts
+++ apps/backend/src/app/auctions/auctions.module.ts
@@ -5,10 +5,11 @@ import { AuctionsController } from './auctions.controller';
 import { Auction } from './entities/auction.entity';
 import { Category } from '../categories/entities/category.entity';
 import { User } from '../users/entities/user.entity';
+import { AuctionExistsRule } from './validators/auction-exists';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Auction, Category, User])],
   controllers: [AuctionsController],
-  providers: [AuctionsService],
+  providers: [AuctionsService, AuctionExistsRule],
 })
 export class AuctionsModule {}
