84a438481f77c79846312b5d132ceeae03c0675f
Pawel Marcinek <pawel.marcinek@intive.com>
2023-04-20 10:45:41 +0200

    INHEART-315: Fix for initial issue with tests not running Removal of SignInPage.spec

--- apps/mobile/jest.config.ts
+++ apps/mobile/jest.config.ts
@@ -3,5 +3,6 @@ module.exports = {
   preset: 'react-native',
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+  transformIgnorePatterns: ['node_modules/(?!react-native|react-router-native)/'],
   setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
 };
