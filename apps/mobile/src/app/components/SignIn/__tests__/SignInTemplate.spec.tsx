c22275767c06f305aea28240f516806b1f8539ff
pmarcinek <pawel.marcinek@intive.com>
2023-05-11 14:45:23 +0200

    INHEART-315: Fix for initial issue with tests not running (#175)
    Removal of SignInPage.spec

--- apps/mobile/src/app/components/SignIn/__tests__/SignInTemplate.spec.tsx
+++ apps/mobile/src/app/components/SignIn/__tests__/SignInTemplate.spec.tsx
@@ -1,10 +1,16 @@
 import { render } from '@testing-library/react-native';
 
 import SignInTemplate from './SignInTemplate';
+import { NativeRouter } from 'react-router-native';
 
 describe('SignInTemplate', () => {
   it('should render successfully', () => {
-    const { container } = render(<SignInTemplate />);
+    const container = render(
+      <NativeRouter>
+        <SignInTemplate />
+      </NativeRouter>
+    );
+
     expect(container).toBeTruthy();
   });
 });
--- apps/mobile/test-setup.ts
+++ apps/mobile/test-setup.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-native/extend-expect';
+import { jest } from '@jest/globals';
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('react-native-device-info', () => mockRNDeviceInfo);
+jest.mock('react-native-keyboard-aware-scroll-view');
+jest.mock('react-native-vector-icons/MaterialIcons', () => 'MaterialIcon');
+jest.mock('react-native-vector-icons/AntDesign', () => 'AntIcon');
+jest.mock('react-native-vector-icons/Ionicons', () => 'Ionicons');
+jest.mock('react-native-vector-icons/Feather', () => 'Feather');
