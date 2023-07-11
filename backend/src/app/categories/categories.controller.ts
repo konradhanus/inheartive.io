3ab2a3583c9e6393de44b09536ee5aae0ec13200
zolkiBy <dev.yaschchuk@gmail.com>
2023-07-11 15:14:22 +0200

    INHEART-511 swagger docs for categories api (#211)

--- backend/src/app/categories/categories.controller.ts
+++ backend/src/app/categories/categories.controller.ts
@@ -1,34 +1,66 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CreateCategoryBody, CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryBody } from './dto/update-category.dto';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CategoryDto } from './dto/category.dto';
 
+@ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly categoriesService: CategoriesService) { }
 
+  @ApiCreatedResponse({ description: 'The category record has been successfully created.', type: CreateCategoryDto })
+  @ApiBadRequestResponse()
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  async create(@Body() createCategoryBody: CreateCategoryBody): Promise<CreateCategoryDto> {
+    const createdCategory = await this.categoriesService.create(createCategoryBody);
+    return CategoriesService.toCreateCategoryDto(createdCategory);
   }
 
+  @ApiResponse({ type: [CategoryDto] })
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  async findAll(): Promise<CategoryDto[]> {
+    const categories = await this.categoriesService.findAll();
+    return categories.map((category) => CategoriesService.toCategoryDto(category));
   }
 
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'should be an id of category that exists in the database',
+    type: String
+  })
+  @ApiResponse({ type: CategoryDto })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(id);
+  async findOne(@Param('id') id: string): Promise<CategoryDto> {
+     const category = await this.categoriesService.findOne(id);
+     return CategoriesService.toCategoryDto(category);
   }
 
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'should be an id of category that exists in the database',
+    type: String
+  })
+  @ApiResponse({ type: CategoryDto })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoriesService.update(id, updateCategoryDto);
+  async update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryBody): Promise<CategoryDto> {
+    const category = await this.categoriesService.update(id, updateCategoryDto);
+    return CategoriesService.toCategoryDto(category);
   }
 
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'should be an id of category that exists in the database',
+    type: String
+  })
+  @ApiResponse({ type: CategoryDto })
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriesService.remove(id);
+  async remove(@Param('id') id: string): Promise<CategoryDto> {
+    const category = await this.categoriesService.remove(id);
+    return CategoriesService.toCategoryDto(category);
   }
 }
--- backend/src/app/categories/dto/create-category.dto.ts
+++ backend/src/app/categories/dto/create-category.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length } from 'class-validator';
+import { CategoryDto } from './category.dto';
 
-export class CreateCategoryDto {
+export class CreateCategoryDto extends CategoryDto { }
+
+export class CreateCategoryBody {
+  @ApiProperty()
   @IsString()
   @Length(2, 30)
   readonly name: string;
