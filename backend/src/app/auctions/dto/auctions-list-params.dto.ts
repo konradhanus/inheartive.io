eeaa4e7f66400e9eb832f0a292fd1497b4252864
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2023-07-14 12:17:59 +0200

    INHEART-510 add query param doc for auctions

--- backend/src/app/auctions/dto/auctions-list-params.dto.ts
+++ backend/src/app/auctions/dto/auctions-list-params.dto.ts
@@ -1,11 +1,23 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { AuctionSorkKey } from '../auctions.types';
 import { FindOptionsOrderValue } from 'typeorm';
 
 export class AuctionsListParamsDto {
+  @ApiPropertyOptional()
   authorId?: string;
+
+  @ApiPropertyOptional()
   categoryId?: string;
+
+  @ApiPropertyOptional()
   isFinished?: string;
+
+  @ApiPropertyOptional()
   sortBy?: AuctionSorkKey;
+
+  @ApiPropertyOptional()
   order?: FindOptionsOrderValue;
+
+  @ApiPropertyOptional()
   bidAuthorId?: string;
 }
