0aff48040ad5f45da9f0c60ab1125ab8ad724b7e
Przemek Musialik <pmusialik@blackberry.com>
2023-07-14 17:25:22 +0200

    INHEART-501 Add SSO login

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -9,36 +9,53 @@ import AuthenticatedPageWrapper from './src/components/AuthenticatedPageWrapper/
 import SignInPage from './src/components/SignIn/SignInPage';
 import { UserProvider, useUser } from './src/components/Providers/UserProvider';
 
+import { MsalProvider } from '@azure/msal-react';
+import { Configuration, PublicClientApplication } from '@azure/msal-browser';
+
+const configuration: Configuration = {
+  auth: {
+    clientId: process.env.AAD_CLIENT_ID as string,
+    authority: `https://login.microsoftonline.com/${process.env.AAD_TENANT_ID}`,
+    redirectUri: 'http://localhost:19006/sso',
+  },
+  cache: {
+    cacheLocation: 'sessionStorage',
+    storeAuthStateInCookie: false,
+  },
+};
+
 export const App = () => {
   const queryClient = new QueryClient();
   const { user } = useUser();
 
+  const pca = new PublicClientApplication(configuration);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NativeBaseProvider theme={theme}>
-        <NativeRouter>
-          <Routes>
-            {routesConfig.map(({ path, needsAuth, page, footerIcon }) => (
-              <Route
-                key={path}
-                path={path}
-                element={
-                  needsAuth ? (
-                    user ? (
-                      <AuthenticatedPageWrapper footerActiveIcon={footerIcon}>
-                        {page}
-                      </AuthenticatedPageWrapper>
+        <MsalProvider instance={pca}>
+          <NativeRouter>
+            <Routes>
+              {routesConfig.map(({ path, needsAuth, page, footerIcon }) => (
+                <Route
+                  key={path}
+                  path={path}
+                  element={
+                    needsAuth ? (
+                      user ? (
+                        <AuthenticatedPageWrapper footerActiveIcon={footerIcon}>{page}</AuthenticatedPageWrapper>
+                      ) : (
+                        <SignInPage />
+                      )
                     ) : (
-                      <SignInPage />
+                      page
                     )
-                  ) : (
-                    page
-                  )
-                }
-              />
-            ))}
-          </Routes>
-        </NativeRouter>
+                  }
+                />
+              ))}
+            </Routes>
+          </NativeRouter>
+        </MsalProvider>
       </NativeBaseProvider>
     </QueryClientProvider>
   );
--- frontend/src/libs/ui/molecules/src/LoginFormControl/LoginFormControl.tsx
+++ frontend/src/libs/ui/molecules/src/LoginFormControl/LoginFormControl.tsx
@@ -1,98 +1,34 @@
-import React, { useState } from 'react';
-import { useMutation } from '@tanstack/react-query';
-import { useForm, FormProvider } from 'react-hook-form';
+import React, { useEffect, useState } from 'react';
 
-import { Button, FormControl, View, Text } from '../../../atoms';
-import { apiRoutes } from '../../../../data';
-import { useNavigate } from 'react-router-native';
-import { EmailInput, PasswordInput } from '../../../organisms';
-import { useUser } from '../../../../../components/Providers/UserProvider';
-import { RoutingPath } from '../../../../../routing/routing-path';
-import { HttpMethods, fetchData, setValue } from '../../../shared/utils';
+import { Button } from '../../../atoms';
+import { useMsal, useAccount } from '@azure/msal-react';
 
-interface LoginForm {
-    email: string;
-    password: string;
-}
 function LoginFormControl() {
-    const { setUser } = useUser();
-    const [loginError, setLoginError] = useState(false);
+  const { instance, accounts } = useMsal();
+  const account = useAccount(accounts[0] || {});
+  console.log({ account });
 
-    const formMethods = useForm<LoginForm>({ mode: 'onChange' });
-    const {
-        getValues,
-        formState: { errors, isValid },
-        handleSubmit,
-    } = formMethods;
+  useEffect(() => {
+    if (account) {
+      instance
+        .acquireTokenSilent({
+          scopes: ['User.Read'],
+          account: account,
+        })
+        .then((response) => {
+          if (response) {
+            // TODO? probably no need
+            // callMsGraph(response.accessToken).then((result) => setApiData(result));
+          }
+        });
+    }
+  }, [account, instance]);
 
-    const navigate = useNavigate();
-
-    const setAccessToken = async (access_token: string) => {
-        await setValue('access_token', access_token);
-    };
-
-    const { mutateAsync } = useMutation({
-        mutationFn: async (data: LoginForm) => {
-            return fetchData(apiRoutes.login, HttpMethods.POST, data);
-        },
-        onSuccess: (value) => {
-            value
-                .json()
-                .then((data) => {
-                    const { access_token = '', user = null } = data || {};
-                    if (access_token) {
-                        setAccessToken(access_token);
-                        setUser(user);
-                        navigate(RoutingPath.auctions);
-                    }
-                })
-                .catch(() => {
-                    setLoginError(true);
-                });
-        },
-    });
-
-    const signIn = () => {
-        const { email, password } = getValues();
-        setLoginError(false);
-        if (isValid) {
-            mutateAsync({ email, password });
-        }
-    };
-
-    const isDisabled = !isValid;
-
-    return (
-        <FormProvider {...formMethods}>
-            <FormControl isInvalid={'email' in errors}>
-                <View>
-                    <EmailInput placeholder='E-mail' />
-                    <View height={6} position='relative' marginBottom={2}>
-                        <FormControl.ErrorMessage>
-                            {errors.email?.message}
-                        </FormControl.ErrorMessage>
-                    </View>
-                </View>
-            </FormControl>
-            <FormControl isInvalid={'password' in errors}>
-                <PasswordInput placeholder='Password' />
-                <View height={6} position='relative' marginBottom={2}>
-                    <FormControl.ErrorMessage>
-                        {errors.password?.message}
-                    </FormControl.ErrorMessage>
-                </View>
-            </FormControl>
-            {loginError && <Text>User name or password are wrong</Text>}
-            <Button
-                mt='4'
-                onPress={(e) => handleSubmit(signIn)(e)}
-                disabled={isDisabled}
-                isDisabled={isDisabled}
-            >
-                Sign in
-            </Button>
-        </FormProvider>
-    );
+  return (
+    <Button onPress={() => instance.loginPopup()} mt='4'>
+      Login
+    </Button>
+  );
 }
 
 export { LoginFormControl };
