06d8768ffb578895e62ffeec1a180fdaa3638d69
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2023-07-17 15:36:12 +0200

    INHEART-514 add swagger docs for auth

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/jwt.guard';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @UseGuards(JwtGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiExcludeEndpoint()
   @Get()
   getData() {
     return this.appService.getData();
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,10 +3,12 @@ import { Response } from 'express';
 import { Oauth2Guard } from './oauth2.guard';
 import { AuthService } from './auth.service';
 import { User } from './types/user';
+import { ApiBody, ApiCreatedResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Login')
 @Controller('login')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @UseGuards(Oauth2Guard)
   @Get('_auth/callback')
@@ -16,6 +18,47 @@ export class AuthController {
   }
   // TODO uncomment this guard
   // @UseGuards(Oauth2Guard)
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        'email': {
+          type: 'string'
+        },
+        'password': {
+          type: 'string'
+        }
+      }
+    }
+  })
+  @ApiResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        'access_token': {
+          type: 'string'
+        },
+        user: {
+          type: 'object',
+          properties: {
+            'id': {
+              type: 'string'
+            },
+            'firstName': {
+              type: 'string'
+            },
+            'lastName': {
+              type: 'string'
+            },
+            'initials': {
+              type: 'string'
+            }
+          }
+        }
+      }
+    }
+  })
+  @ApiCreatedResponse()
   @Post()
   async login(@Request() req: { body: User }) {
     const { body: user } = req;
