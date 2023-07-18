43ee5b2c5ee60b784bd53097b6f1dd23cd5d6d12
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2023-07-18 14:24:01 +0200

    INHEART-512 swagger doc for bids

--- backend/src/app/bids/bids.controller.ts
+++ backend/src/app/bids/bids.controller.ts
@@ -2,16 +2,20 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BidsService } from './bids.service';
 import { CreateBidBody } from './dto/create-bid.dto';
 import { BidDto } from './dto/bid.dto';
+import { ApiCreatedResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Bids')
 @Controller('bids')
 export class BidsController {
   constructor(private readonly bidsService: BidsService) { }
 
+  @ApiCreatedResponse({ description: 'The bid record has been successfully created.' })
   @Post()
-  create(@Body() createBidDto: CreateBidBody): Promise<void> {
-    return this.bidsService.create(createBidDto);
+  create(@Body() createBidBody: CreateBidBody): Promise<void> {
+    return this.bidsService.create(createBidBody);
   }
 
+  @ApiResponse({ type: [BidDto] })
   @Get()
   async findAll(): Promise<BidDto[]> {
     const bids = await this.bidsService.findAll();
--- backend/src/app/bids/dto/create-bid.dto.ts
+++ backend/src/app/bids/dto/create-bid.dto.ts
@@ -1,17 +1,21 @@
 import { IsInt, IsPositive, IsUUID, Max, Validate } from 'class-validator';
 import { UserExistsRule } from '../../users/validators/user-exists';
 import { AuctionExistsRule } from '../../auctions/validators/auction-exists';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBidBody {
+  @ApiProperty()
   @IsPositive()
   @Max(parseInt(process.env.MAX_BID_VALUE))
   @IsInt()
   readonly value: number;
 
+  @ApiProperty()
   @IsUUID()
   @Validate(AuctionExistsRule)
   readonly auction: string;
 
+  @ApiProperty()
   @IsUUID()
   @Validate(UserExistsRule)
   readonly user: string;
