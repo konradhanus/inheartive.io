cad3980819d082f474cb703d528a450bf822d765
robert.grembowicz <robert.grembowicz@192.168.1.7>
2023-08-09 13:58:45 +0200

    adding max width to select in AppHeader

--- frontend/src/libs/ui/organisms/src/AppHeader/AppHeader.tsx
+++ frontend/src/libs/ui/organisms/src/AppHeader/AppHeader.tsx
@@ -1,64 +1,59 @@
 import React, { useState } from 'react';
-import { Avatar, Row, TextLogo, textLogoColor, View, Select } from '../../../atoms';
-import { useNavigate } from 'react-router-native';
-import { RoutingPath } from '../../../../../routing/routing-path';
-import { setValue } from '../../../shared/utils';
-import { useUser } from '../../../../../components/Providers/UserProvider';
-import { Item } from '../../../atoms/src/Select/SelectItem';
 import { Platform } from 'react-native';
-
+import { useNavigate } from 'react-router-native';
+import { useUser } from '../../../../../components/Providers/UserProvider';
+import { RoutingPath } from '../../../../../routing/routing-path';
+import {
+  Avatar,
+  Row,
+  Select,
+  TextLogo,
+  View,
+  textLogoColor,
+} from '../../../atoms';
+import { Item } from '../../../atoms/src/Select/SelectItem';
+import { setValue } from '../../../shared/utils';
 
 function AppHeader() {
-    const [menuItem, setMenuItem] = useState('');
-    const navigate = useNavigate();
-    const { setUser } = useUser();
-    const logOut = () => {
-        setValue('access_token', '');
-        setUser(null);
-        navigate(RoutingPath.signIn);
-    };
+  const [menuItem, setMenuItem] = useState('');
+  const navigate = useNavigate();
+  const { setUser } = useUser();
+  const logOut = () => {
+    setValue('access_token', '');
+    setUser(null);
+    navigate(RoutingPath.signIn);
+  };
 
-    const toMyAuctions = () => navigate(RoutingPath.myAuctions);
-    const toMyBids = () => navigate(RoutingPath.myBids);
+  const toMyAuctions = () => navigate(RoutingPath.myAuctions);
+  const toMyBids = () => navigate(RoutingPath.myBids);
 
-    return (
-        <Row
-            px={8}
-            pt={Platform.OS === "ios" ? 10 : 8}
-            pb={2}
-            justifyContent={'space-between'}
-            bg='primary.500'
-            alignItems='center'
+  return (
+    <Row
+      px={8}
+      pt={Platform.OS === 'ios' ? 10 : 8}
+      pb={2}
+      justifyContent={'space-between'}
+      bg="primary.500"
+      alignItems="center"
+    >
+      <TextLogo width={150} color={textLogoColor.inverted} />
+      <View>
+        <Select
+          borderColor={'transparent'}
+          selectedValue={menuItem}
+          onValueChange={(itemValue) => setMenuItem(itemValue)}
+          dropdownIcon={<Avatar size={'lg'} />}
+          display="flex"
+          flexGrow={1}
+          maxW="140"
         >
-            <TextLogo width={150} color={textLogoColor.inverted} />
-            <View>
-                <Select
-                    borderColor={'transparent'}
-                    selectedValue={menuItem}
-                    onValueChange={(itemValue) => setMenuItem(itemValue)}
-                    dropdownIcon={<Avatar size={'lg'} />}
-                    display='flex'
-                    flexGrow={1}
-                >
-                    <Item
-                        label='Log out'
-                        value='Logout'
-                        onPress={logOut}
-                    />
-                    <Item
-                        label='My auctions'
-                        value='MyAuctions'
-                        onPress={toMyAuctions}
-                    />
-                    <Item
-                        label='My bids'
-                        value='MyBids'
-                        onPress={toMyBids}
-                    />
-                </Select>
-            </View>
-        </Row>
-    );
+          <Item label="Log out" value="Logout" onPress={logOut} />
+          <Item label="My auctions" value="MyAuctions" onPress={toMyAuctions} />
+          <Item label="My bids" value="MyBids" onPress={toMyBids} />
+        </Select>
+      </View>
+    </Row>
+  );
 }
 
 export { AppHeader };
