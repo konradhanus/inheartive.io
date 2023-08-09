3569d4883b21af0b0a66dcf4c5f79586c39661d0
robert.grembowicz <robert.grembowicz@192.168.1.7>
2023-08-09 13:43:53 +0200

    adding top shadow to bottom menu

--- frontend/src/libs/ui/organisms/src/AppFooter/AppFooter.tsx
+++ frontend/src/libs/ui/organisms/src/AppFooter/AppFooter.tsx
@@ -1,76 +1,92 @@
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
 
+import { HStack, View } from 'native-base';
 import React from 'react';
-import { HeartsCredits } from '../../../molecules';
-import { HStack } from 'native-base';
+import { Platform, StyleSheet } from 'react-native';
 import { Link } from 'react-router-native';
-import { FooterIcon, FooterIconRoutingMap } from './footer-icon';
+import { HeartsCredits } from '../../../molecules';
 import { AppFooterIcon } from './AppFooterIcon';
+import { FooterIcon, FooterIconRoutingMap } from './footer-icon';
 
 interface Props {
-    activeIcon: FooterIcon;
-    iconRoutingMap: FooterIconRoutingMap;
+  activeIcon: FooterIcon;
+  iconRoutingMap: FooterIconRoutingMap;
 }
 
 function AppFooter(props: Props) {
-    const { activeIcon, iconRoutingMap } = props;
+  const { activeIcon, iconRoutingMap } = props;
 
-    return (
-        <HStack
-            bg='white'
-            justifyContent='space-between'
-            px='4'
-            alignItems='center'
-            testID='footer-menu'
+  return (
+    <View style={Platform.OS === 'android' && style.container}>
+      <HStack
+        bg="white"
+        justifyContent="space-between"
+        px="4"
+        alignItems="center"
+        testID="footer-menu"
+        style={style.innerContainer}
+      >
+        <Link
+          to={iconRoutingMap[FooterIcon.auctions]}
+          underlayColor="transparent"
         >
-            <Link
-                to={iconRoutingMap[FooterIcon.auctions]}
-                underlayColor='transparent'
-            >
-                <AppFooterIcon
-                    name={FooterIcon.auctions}
-                    isActive={activeIcon === FooterIcon.auctions}
-                />
-            </Link>
+          <AppFooterIcon
+            name={FooterIcon.auctions}
+            isActive={activeIcon === FooterIcon.auctions}
+          />
+        </Link>
 
-            <Link
-                to={iconRoutingMap[FooterIcon.auctions]}
-                underlayColor='transparent'
-            >
-                <AppFooterIcon
-                    name={FooterIcon.search}
-                    isActive={activeIcon === FooterIcon.search}
-                />
-            </Link>
+        <Link
+          to={iconRoutingMap[FooterIcon.auctions]}
+          underlayColor="transparent"
+        >
+          <AppFooterIcon
+            name={FooterIcon.search}
+            isActive={activeIcon === FooterIcon.search}
+          />
+        </Link>
 
-            <Link
-                to={iconRoutingMap[FooterIcon.auctions]}
-                underlayColor='transparent'
-            >
-                <HeartsCredits credit={99} size={60} />
-            </Link>
+        <Link
+          to={iconRoutingMap[FooterIcon.auctions]}
+          underlayColor="transparent"
+        >
+          <HeartsCredits credit={99} size={60} />
+        </Link>
 
-            <Link
-                to={iconRoutingMap[FooterIcon.addAuction]}
-                underlayColor='transparent'
-            >
-                <AppFooterIcon
-                    name={FooterIcon.addAuction}
-                    isActive={activeIcon === FooterIcon.addAuction}
-                />
-            </Link>
+        <Link
+          to={iconRoutingMap[FooterIcon.addAuction]}
+          underlayColor="transparent"
+        >
+          <AppFooterIcon
+            name={FooterIcon.addAuction}
+            isActive={activeIcon === FooterIcon.addAuction}
+          />
+        </Link>
 
-            <Link
-                to={iconRoutingMap[FooterIcon.auctions]}
-                underlayColor='transparent'
-            >
-                <AppFooterIcon
-                    name={FooterIcon.favorites}
-                    isActive={activeIcon === FooterIcon.favorites}
-                />
-            </Link>
-        </HStack>
-    );
+        <Link
+          to={iconRoutingMap[FooterIcon.auctions]}
+          underlayColor="transparent"
+        >
+          <AppFooterIcon
+            name={FooterIcon.favorites}
+            isActive={activeIcon === FooterIcon.favorites}
+          />
+        </Link>
+      </HStack>
+    </View>
+  );
 }
+
+const style = StyleSheet.create({
+  container: { elevation: 1, paddingTop: 5 },
+  innerContainer: {
+    shadowRadius: 2,
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 0,
+      height: -3,
+    },
+  },
+});
 
 export { AppFooter };
